4e09e800ec7f25121c5230518905e451dd3d9c3b
jloggerr <33246273+jloggerr@users.noreply.github.com>
2017-10-31 09:54:56 +0330

    Create ip.py

---
+++ ip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#Coded By J_Logger
+# This Script Is IP FOUNDER
+import socket
+
+print ""
+   IP Founder
+   
+      ""
+e = str(raw_input(" url >>> "))
+s = socket.gethostbyname(e)
+print ""
+ IP HOST \"%s IS %s
+      ""% (e, s)
